f648190d05dbab1a6ef851eaa1c7d25763ad0526
bosstom5201 <goldenstardev55@outlook.com>
2012-03-30 12:12:00 -0400

    cleanup

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 # M=$[5 + $RANDOM % 3]
 # echo $M
-for Y in {2011..2011}
+for Y in {2012..2012}
 do
   mkdir $Y
   cd $Y
-  for M in {08..11}
+  for M in {01..03}
     do
       # if [ M != $[5 + $RANDOM % 3] ]; then
       # M=$[5 + $RANDOM % 3]
